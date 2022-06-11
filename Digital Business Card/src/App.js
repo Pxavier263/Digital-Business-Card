2b29116fd60410676d91ed841b786b588185fbc5
Pxavier <Pxavier263@gmail.com>
2022-06-11 20:20:14 +0100

    adding particles

--- Digital Business Card/src/App.js
+++ Digital Business Card/src/App.js
@@ -3,14 +3,44 @@ import Introduction from './Components/Introduction';
 import About from './Components/About';
 import Footer from './Components/Footer';
 import Interests from './Components/Interest';
+import { Particles } from '@blackbox-vision/react-particles';
+import './App.css';
 
-import './App.css'
-
-
+const ParticlesJs = () => (
+    <Particles
+        style={{
+        position: 'fixed',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: '-1',
+      }}
+      params={{
+        particles: {
+          number: {
+            value: 250,
+          },
+          size: {
+            value: 3,
+          },
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: 'repulse',
+            },
+          },
+        },
+      }}
+    />
+  );  
 
 function App() {
     return (
            <div>
+           <ParticlesJs className='particles'/>
                 <Introduction />
                 <About />
                 <Interests/>
@@ -19,57 +49,3 @@ function App() {
     )
 };
 export default App;
-
-// <div className="contact-card">
-// <img src="./images/felix.png"/>
-// <h3>Felix</h3>
-// <div className="info-group">
-//     <img src="./images/phone-icon.png" />
-//     <p>(212) 555-4567</p>
-// </div>
-// <div className="info-group">
-//     <img src="./images/mail-icon.png" />
-//     <p>thecat@hotmail.com</p>
-// </div>
-// </div>
-
-// <div className="contact-card">
-// <img src="./images/pumpkin.png"/>
-// <h3>Pumpkin</h3>
-// <div className="info-group">
-//     <img src="./images/phone-icon.png" />
-//     <p>(0800) CAT KING</p>
-// </div>
-// <div className="info-group">
-//     <img src="./images/mail-icon.png" />
-//     <p>pumpkin@scrimba.com</p>
-// </div>
-// </div>
-
-// <div className='contacts'>        
-// <Contacts 
-// img= {image}
-// name= 'Fluffykins'
-// phone='(212) 555-2345' 
-// email='fluff@me.com'
-// ispun= {true}
-// upvote = {100} />
-
-// <Contacts 
-// img= {image1}
-// name= 'Felix'
-// phone='(212) 555-4567' 
-// email='thecat@hotmail.com'/>
-
-// <Contacts 
-// img= {image2}
-// name= 'Pumpkin'
-// phone='(0800) CAT KING' 
-// email='pumpkin@scrimba.com'/>
-
-// <Contacts 
-// img= {image3}
-// name= 'Fluffykins'
-// phone='(212) 555-2345' 
-// email='fluff@me.com'/>
-// </div>
